236897100cedef8f77f81435d92f70b5dc24e04d
Elena <littleyoru2000@gmail.com>
2020-10-27 10:32:02 +0100

    added elements for the first page

--- petapp/App.js
+++ petapp/App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableHighlight style={styles.btn}>
+        <Text style={styles.btnText}>Report missing pet</Text>
+      </TouchableHighlight>
+      <TouchableHighlight style={styles.btn}>
+        <Text style={styles.btnText}>Search missing pets</Text>
+      </TouchableHighlight>
     </View>
   );
 }
@@ -17,5 +21,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  btn: {
+    backgroundColor: '#C1D90B',
+    width: '70%',
+    marginBottom: 16,
+    padding: 16,
+    borderRadius: 5,
+    shadowOffset: { width: 5, height: 10, },
+    shadowColor: '#000000',
+    shadowOpacity: 0.8,
+    shadowRadius: 5,
+    elevation: 1
+  },
+  btnText: {
+    fontSize: 18,
+    fontWeight: '700',
+    textAlign: 'center'
   },
 });
